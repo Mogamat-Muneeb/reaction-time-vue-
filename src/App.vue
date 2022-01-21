0855a95f663ce01dabd22c7556752cd9b3f32373
Mogamat-Muneeb <davimuneeb785@gmail.com>
2022-01-21 11:39:28 +0200

    done

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,38 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Muneeb Reaction Timer</h1>
+  <button @click="start" :disabled="isPLaying">play</button>
+  <Block v-if="isPLaying" :delay="delay" @end="endGame"/>
+  <Results v-if="showResults" :score="score"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Block from "./components/Block.vue"
+import Results from "./components/Results.vue"
+
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: { Block, Results },
+  data(){
+    return{
+      isPLaying:false,
+      delay:null,
+      score:null,
+      showResults: false
+    }
+  },
+  methods:{
+    start(){
+      this.delay = 2000 + Math.random() * 5000 
+      this.isPLaying = true
+    
+    },
+    endGame(reactionTime){
+      this.score = reactionTime
+      this.isPLaying = false
+      this.showResults = true
+      
+    }
   }
 }
 </script>
@@ -20,7 +43,22 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #444;
   margin-top: 60px;
+}
+button{
+  background:violet ;
+  color: #444;
+  border: none;
+  padding: 8px 16px;
+  border-radius:4px ;
+  font-size: 16px;
+  letter-spacing: 1px;
+  cursor: pointer;
+  margin: 10px;
+}
+button[disabled]{
+  opacity: 0.2;
+  cursor: not-allowed;
 }
 </style>
